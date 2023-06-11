bfef9e079a958f852c8d0a0d58bfd34869ee16ba
Andrei <asd@mail.com>
2023-06-11 16:33:57 +0300

    Added constructor method

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -9,11 +9,29 @@ public class TicTacToe implements ActionListener {
     JFrame frame = new JFrame();
     JPanel title_panel = new JPanel();
     JPanel button_panel = new JPanel();
-    JLabel text_field = new JLabel();
+    JLabel textfield = new JLabel();
     JButton[] buttons = new JButton[9];
     boolean player1;
-    
+
     public TicTacToe() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 800);
+        frame.getContentPane().setBackground(new Color(50, 50, 50));
+        frame.setLayout(new BorderLayout());
+        frame.setVisible(true);
+
+        textfield.setBackground(new Color(25,25, 25));
+        textfield.setForeground(new Color(25, 250, 0));
+        textfield.setFont(new Font("ink Free", Font.BOLD, 75));
+        textfield.setHorizontalAlignment(JLabel.CENTER);
+        textfield.setText("Tic Tac Toe");
+        textfield.setOpaque(true);
+
+        title_panel.setLayout(new BorderLayout());
+        title_panel.setBounds(0, 0, 800, 100);
+
+        title_panel.add(textfield);
+        frame.add(title_panel, BorderLayout.NORTH);
     }
 
     @Override
